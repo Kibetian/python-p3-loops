4c4f2fcb5751770e9b64331be5ad60874ec5ce19
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-08-19 14:25:42 +0300

    DONE

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!") 
+
+# happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_int_list =[num ** 2 for num in int_list if isinstance(num, int)]
+    return squared_int_list
+# int_list = [1, 2, 3, 4, 5]
+# squared_int_list = square_integers(int_list)
+# print(squared_int_list)
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0 :
+            print ('Fizz')
+        elif num % 5 == 0:
+            print ("Buzz")
+        else:
+            print(num)
+# fizzbuzz()
